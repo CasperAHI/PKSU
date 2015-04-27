fbe75f921868444fd453a0df6616c26bd197c289
sorenlundj <Sorenlundj@gmail.com>
2015-04-27 15:48:28 +0200

    Metoder

---
+++ Metoder.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+db = sqlite3.connect('/Users/Soren/Desktop/Udlånssystem/KEAbib.db')
+c = db.cursor()
+
+def ledige():
+    l = c.execute('SELECT deviceid FROM Device WHERE (SELECT did FROM Loan WHERE delivered != null').fetchall()
+    return l
+
+def udlaant():
+    u = c.execute('SELECT deviceid FROM Device WHERE (SELECT did FROM Loan WHERE delivered = null').fetchall()
+    return u
+    
+def udlaan(computerNavn, brugerNavn):
+    c.execute('INSERT INTO Loan VALUES (SELECT brugerid FROM Bruger WHERE navn = brugerNavn, SELECT deviceid FROM Device WHERE navn = deviceNavn,now())')
+
+def aflever(computerNavn):
+    c.execute('UPDATE Loan SET delivered = timestamp WHERE did = (SELECT deviceid FROM Device WHERE navn = computerNavn)')
+    
+def brugerHistorik(brugerNavn):
+    h = c.execute('SELECT * FROM Bruger WHERE navn = brugerNavn').fetchall
+    return h
+
+def computerHistorik(deviceNavn):
+    h = c.execute('SELECT * FROM Device WHERE navn = deviceNavn').fetchall
+    return h
+
+print(3)
+print (udlaant())
