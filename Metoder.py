99c0d55f83562e982fd4be0d3bfb55775db55833
Andeky <Bimmeren@Andreass-MacBook-Pro-3.local>
2015-04-27 17:20:58 +0200

    Indsættelse af bruger og enhed i DB

--- Metoder.py
+++ Metoder.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import sqlite3
 
 db = sqlite3.connect('KEAbib.db')
@@ -13,7 +12,7 @@ def udlaant():
     return u
     
 def udlaan(computerNavn, brugerNavn):
-    c.execute('INSERT INTO Loan VALUES (SELECT brugerid FROM Bruger WHERE navn = brugerNavn, SELECT deviceid FROM Device WHERE navn = deviceNavn,now())')
+    c.execute('INSERT INTO Loan(bid,did,loan) VALUES ((SELECT brugerid FROM Bruger WHERE navn = brugerNavn), (SELECT deviceid FROM Device WHERE navn = computerNavn),now())')
 
 def aflever(computerNavn):
     c.execute('UPDATE Loan SET delivered = timestamp WHERE did = (SELECT deviceid FROM Device WHERE navn = computerNavn)')
@@ -26,5 +25,10 @@ def computerHistorik(deviceNavn):
     h = c.execute('SELECT * FROM Device WHERE navn = deviceNavn').fetchall
     return h
 
-print(3)
-print (ledige())
+def indsaetDevice(typen,navne,tyveride):
+    c.execute('''INSERT INTO Device(type, navn, tyverid)
+                  VALUES(?,?,?)''', (typen,navne,tyveride))
+
+def indsaetBruger(maile,navne,tele):
+    c.execute('''INSERT INTO Bruger(mail,navn,telefon)
+                  VALUES(?,?,?)''', (maile,navne,tele))
